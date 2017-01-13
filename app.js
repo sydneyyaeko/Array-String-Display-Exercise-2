e348d4bd9efc2102f16bb50b88efe57168a81b12
Sydney Yaeko <sydneyrodriguera@gmail.com>
2017-01-12 21:13:29 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+/* If you were to look up array two, it would be Mary and not Tom.
+This is because array is at 0 and 0 is Bob, making Tom 1 and Mary 2 Woo!*/
+var myArray = ["Bob", "Tom", "Mary", "Jim"]
+
+
+
+
+
+myArray.push("Nick");
+
+console.log(myArray);
+
+myArray.pop();
+
+console.log(myArray);
+
+myArray.unshift('Brian');
+
+console.log(myArray);
+
+myArray.shift();
+
+console.log(myArray);
